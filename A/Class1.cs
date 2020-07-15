1f97dd4094c000ba551bdd224ce6dcdba0026e5d
William Rummler <w.a.rummler@gmail.com>
2020-07-15 14:31:10 -0500

    Add project files.

---
+++ A/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace A
+{
+    public class Class1
+    {
+        public string GetHelloB()
+        {
+            var executableB = typeof(B.Program).Assembly.Location;
+            var info = new ProcessStartInfo(executableB)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            };
+            using var p = Process.Start(info);
+            var hello = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return hello;
+        }
+    }
+}
---
+++ DC.VS.Problem_1111582/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DC.VS.Problem_1111582
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new A.Class1().GetHelloB()); // Error
+        }
+    }
+}
